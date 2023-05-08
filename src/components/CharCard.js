274c44e72f253536b614c02f185407170f43fd0b
Gort <126199575+GortFan@users.noreply.github.com>
2023-05-08 14:32:44 -0400

    hi i am here to say that idk what htis is and please send help

--- src/components/CharCard.js
+++ src/components/CharCard.js
@@ -1,7 +1,6 @@
 import classes from './CharCards.module.css'
 
 export default function CharCards(props){
-    //console.log(props)
     return(
         <div className={classes.card}>
             <img src={props.path} alt=''/>
