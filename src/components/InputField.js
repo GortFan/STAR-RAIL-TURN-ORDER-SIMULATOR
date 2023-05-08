e71d66e9cabc24c942f117af85fc670c4ac9e23d
Gort <126199575+GortFan@users.noreply.github.com>
2023-05-08 14:31:45 -0400

    cleaning up code for github

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,58 +1,51 @@
+import CharCardsMapper from './CharCardsMapper'
+import React, {useState} from 'react'
 export default function InputField(props){
 
-  var TurnOrder = props.TurnOrder; 
+  const [array, setArray] = useState(props.TurnOrder)
+  console.log('default: ')
+  console.log(array)
         function handleSubmit(e) {
           // Prevent the browser from reloading the page
           e.preventDefault();
       
-          // Read the form data
           const form = e.target;
           const formData = new FormData(form);
          
-          // Or you can work with it as a plain object:
           const formJson = Object.fromEntries(formData.entries());
-          console.log(formJson);
 
-          //testing math
           let speed = parseInt(formJson.Speed);
-          console.log("The speed is " + speed);
 
           //validation
 
-          if(speed <= 0){
-            console.log('error');
+          if(speed <= 0 || isNaN(speed)){
+            alert('invalid input');
           }
 
           else{
             let AV = 10000/speed;
             if(AV-Math.floor(AV)>0){
-                console.log('float');
                 AV = Math.ceil(AV)
-                console.log('AV is ' + AV)
-                setArray(AV)
+                setOrder(AV)
             }
             else{
-                console.log('int');
-                console.log('AV is ' + AV)
-                setArray(AV)
+
+                setOrder(AV)
             }
           }
         }
 
-        function setArray(AV){
-          TurnOrder[0].AV = AV
-          bubbleSort(TurnOrder)
+        function setOrder(AV){
+          var editArray = [...array]
+          var bronyaIndex = editArray.findIndex(object => object.id === 1)
+          //console.log(bronyaIndex)
+          editArray[bronyaIndex].AV = AV
+          bubbleSort(editArray)
         }
 
-        function bubbleSort(TurnOrder) {
-          // console.log('hi')
-          // console.log(TurnOrder[0].AV)
-          // console.log(TurnOrder[1].AV)
-          // console.log(TurnOrder[2].AV)
-          // console.log(TurnOrder[3].AV)
-
+        function bubbleSort(editArray) {
           var i, j;
-          var len = TurnOrder.length;
+          var len = editArray.length;
         
           var isSwapped = false;
         
@@ -61,37 +54,36 @@ export default function InputField(props){
               isSwapped = false;
         
               for (j = 0; j < len-1; j++) {
-                  if (TurnOrder[j].AV > TurnOrder[j + 1].AV) {
-                      var temp = TurnOrder[j].AV
-                      TurnOrder[j].AV = TurnOrder[j + 1].AV;
-                      TurnOrder[j + 1].AV = temp;
+                  if (editArray[j].AV > editArray[j + 1].AV) {
+                      var temp = editArray[j]
+                      editArray[j] = editArray[j + 1];
+                      editArray[j + 1] = temp;
                      isSwapped = true;
                   }
               }
-        
-              // IF no two elements were swapped
-              // by inner loop, then break 
+
               if (!isSwapped) {
                   break;
               }
           }
         
-          // Print the array
-          console.log(TurnOrder)
+          setArray(editArray)
+          //console.log(array)
         }
-        
-      
-        
-      // calling the bubbleSort Function
-      // bubbleSort(TurnOrder)
+
+        React.useEffect(()=>{
+          console.log('Re-render')
+        }, [array])
       
       
         return (
-          <form method="post" onSubmit={handleSubmit}>
-            <input type='number' name="Speed"/>
-            {/* <input name="Gauge"/>
-            <input name="AV"/> */}
-            <button type="submit">Submit form</button>
-          </form>
-        );
+          <div>
+            <form method="post" onSubmit={handleSubmit}>
+              <input type='number' name="Speed"/>
+              <button type="submit">Submit form</button>
+            </form>
+            <CharCardsMapper
+              TurnOrder = {array}/>
+          </div>
+          );
       }
