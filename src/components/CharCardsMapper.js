effccdbcbb38b8b0d1bfd42c387ae0d9a5612859
Gort <126199575+GortFan@users.noreply.github.com>
2023-05-08 14:33:57 -0400

    idk bro

--- src/components/CharCardsMapper.js
+++ src/components/CharCardsMapper.js
@@ -1,7 +1,6 @@
 import CharCard from './CharCard'
 
 export default function CharCardsMapper(props){
-    //console.log(props)
     return(
         <ul>
             {props.TurnOrder.map(e=><CharCard key={e.id} path={e.path}/>)}
