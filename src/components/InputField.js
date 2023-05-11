054d7831a94d5e38d49a2aeee85ac35c24550ae7
Gort <126199575+GortFan@users.noreply.github.com>
2023-05-11 15:48:52 -0400

    added turn advance and reset feature
    its kinda broken still but all good

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -7,54 +7,89 @@ export default function InputField(props){
   const [selected, setSelected] = useState(false);
   const [val, setVal] = useState();
 
-        function handleSubmit(e) {
-          // Prevent the browser from reloading the page
-          e.preventDefault();
-      
-          const form = e.target;
-          const formData = new FormData(form);
-         
-          const formJson = Object.fromEntries(formData.entries());
-
+        function changeSpeed(e) {
           //validation
-          if(formJson.Speed <= 0 || isNaN(formJson.Speed) || formJson.Speed > 999){
+          if(e.target.value <= 0 || isNaN(e.target.value) || e.target.value > 999){
             alert('invalid input');
           }
           else{
-            Calc(formJson)
+            calcChar(e.target.value)
           }
         }
 
-        function Calc(formJson){
+        function calcChar(speed){
           //Character Math
           let editArray = [...array]
           let charIndex = editArray.findIndex(object => object.id === 1)
-          editArray[charIndex].speed = formJson.Speed
-          let charSpeed = editArray[charIndex].speed
-          let AV = Math.ceil(10000/charSpeed)
-          editArray[charIndex].AV = AV
-          //Enemy Math NOTE: FIX THIS WHOEVER ACTUALLY KNOWS THE FORMULA 
+          editArray[charIndex].speed = speed
+          editArray[charIndex].AV = Math.ceil(10000/editArray[charIndex].speed)
+          bubbleSort(editArray)
+        }
+
+        function calcEnemy(debuff){
+          let editArray = [...array]
           let enemyIndex = editArray.findIndex(object => object.id === 5)
-          console.log(editArray[enemyIndex].speed)
-          if(formJson.Break === 'Break'){
-            let enemySpeed = editArray[enemyIndex].speed 
-            let enemyGauge = editArray[enemyIndex].Gauge = 10000+(10000*(0.25))
-            editArray[enemyIndex].AV = Math.ceil(enemyGauge/enemySpeed)
+          if(debuff === 'Break'){
+            editArray[enemyIndex].Gauge = 10000+(10000*(0.25))
+            editArray[enemyIndex].AV = Math.ceil(editArray[enemyIndex].Gauge/editArray[enemyIndex].speed )
           }
-          if(formJson.Imprisonment === 'Imprisonment'){
-            let enemySpeed = (editArray[enemyIndex].speed)*(1-0.1)
-            let enemyGauge = editArray[enemyIndex].Gauge = 10000+(10000*(0.3*(1+0.15))) //0.15 is break effect %
-            editArray[enemyIndex].AV = Math.ceil(enemyGauge/enemySpeed)
+          if(debuff === 'Imprisonment'){
+            editArray[enemyIndex].speed = Math.ceil((editArray[enemyIndex].speed)*(1-0.1))
+            editArray[enemyIndex].Gauge = 10000+(10000*(0.3*(1+0.15))) //0.15 is break effect %
+            editArray[enemyIndex].AV = Math.ceil(editArray[enemyIndex].Gauge/editArray[enemyIndex].speed)
           }
-          if(formJson.Entanglement === 'Entanglement'){
-            let enemySpeed = editArray[enemyIndex].speed
-            let enemyGauge = editArray[enemyIndex].Gauge = 10000+(10000*(0.3*(1+0.15))) //0.15 is break effect %
-            editArray[enemyIndex].AV = Math.ceil(enemyGauge/enemySpeed)
+          if(debuff === 'Entanglement'){
+            editArray[enemyIndex].Gauge = 10000+(10000*(0.3*(1+0.15))) //0.15 is break effect %
+            editArray[enemyIndex].AV = Math.ceil(editArray[enemyIndex].Gauge/editArray[enemyIndex].speed)
           }
-          if(formJson.Break === undefined && formJson.Imprisonment === undefined && formJson.Entanglement === undefined){
+          if(selected === true){
             editArray[enemyIndex].AV = 80
+            editArray[enemyIndex].Gauge = 10000
+            editArray[enemyIndex].speed = 125
           }
-          console.log(formJson.Imprisonment)
+          bubbleSort(editArray)
+        }
+
+        function handleAdvance(){
+          let editArray = [...array]
+          console.log(editArray[0].AV)
+          let multiplier = editArray[0].AV
+          //1st element
+          editArray[0].Gauge = 10000
+          editArray[0].AV = Math.ceil(editArray[0].Gauge/editArray[0].speed)
+          //2nd element
+          editArray[1].Gauge = editArray[1].Gauge - (editArray[1].speed*multiplier)
+          editArray[1].AV = Math.ceil(editArray[1].Gauge/editArray[1].speed)
+          //3rd element
+          editArray[2].Gauge = editArray[2].Gauge - (editArray[2].speed*multiplier)
+          editArray[2].AV = Math.ceil(editArray[2].Gauge/editArray[2].speed)
+          //4th element
+          editArray[3].Gauge = editArray[3].Gauge - (editArray[3].speed*multiplier)
+          editArray[3].AV = Math.ceil(editArray[3].Gauge/editArray[3].speed)
+          //5th element
+          editArray[4].Gauge = editArray[4].Gauge - (editArray[4].speed*multiplier)
+          editArray[4].AV = Math.ceil(editArray[4].Gauge/editArray[4].speed)
+          bubbleSort(editArray)
+        }
+
+        function handleReset(){
+          let editArray = [...array]
+          //1st element
+          editArray[0].Gauge = 10000
+          editArray[0].AV = Math.ceil(editArray[0].Gauge/editArray[0].speed)
+
+          //2nd element
+          editArray[1].Gauge = 10000
+          editArray[1].AV = Math.ceil(editArray[1].Gauge/editArray[1].speed)
+          //3rd element
+          editArray[2].Gauge = 10000
+          editArray[2].AV = Math.ceil(editArray[2].Gauge/editArray[2].speed)
+          //4th element
+          editArray[3].Gauge = 10000
+          editArray[3].AV = Math.ceil(editArray[3].Gauge/editArray[3].speed)
+          //5th element
+          editArray[4].Gauge = 10000
+          editArray[4].AV = Math.ceil(editArray[4].Gauge/editArray[4].speed)
           bubbleSort(editArray)
         }
 
@@ -62,6 +97,7 @@ export default function InputField(props){
         function handleChange(e){
           setSelected(!selected)
           setVal(e.target.value)
+          calcEnemy(e.target.value)
         }
 
         //Sorting array by AV
@@ -92,18 +128,16 @@ export default function InputField(props){
           setArray(editArray)
         }
 
-        React.useEffect(()=>{
-        }, [array])
 
         return (
           <div className={classes.container}>
 
             <div className={classes.formflex}>
-            <form method="post" onSubmit={handleSubmit}>
-
+              <div>
+              
               <div className={classes.speedflex}>
               <p className={classes.parameters}>Change Speed</p>
-              <input type='number' name="Speed"/>
+              <input type='number' name="Speed" onChange={changeSpeed}/>
               </div>
 
               <div className={classes.delayflex}>
@@ -122,10 +156,14 @@ export default function InputField(props){
               </div>
 
               <div className={classes.flexbtn}>
-              <button type="submit">Apply</button>
+              <div>
+              <button onClick={handleAdvance}>Advance Turn</button>
+              <button onClick={handleReset}>Reset</button>
+              </div>
+              </div>
+              
               </div>
 
-            </form>
             </div>
 
             <div>
